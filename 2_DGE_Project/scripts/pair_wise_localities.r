b06f4b2894be22031ebf14e8016a25b5b6188799
Perry Van Wagoner <vanwper@login02.rc.byu.edu>
2026-09-23 17:33:27 -0600

    created script for pairwise comparison

---
+++ 2_DGE_Project/scripts/pair_wise_localities.r
@@ -0,0 +1,60 @@
+library(DESeq2)
+library(dplyr)
+
+#Assign the command line args
+args <- commandArgs(trailingOnly = TRUE)
+counts_file <- args[1]
+sample_csv <- args[2]
+output <- args[3]
+
+#read in the featurecounts files
+raw_counts <- read.delim(counts_file, header = TRUE, skip = 1, check.names = FALSE)
+
+raw_counts <- raw_counts |>
+    rename_with(~ sub("\\..*$", "", basename(.x)), .cols = 7:ncol(raw_counts))
+
+#read in the csv with sample data
+sample_data <- read.csv(sample_csv, stringsAsFactors = FALSE) |>
+    rename(SampleID = 1, Sample = 2, Tissue = 3, State = 4, County = 5)
+
+
+#get just skeletal muscle data
+sm_info <- sample_data |>
+    filter(Tissue == "SM") |>
+    mutate(County = relevel(factor(County), ref = "Culberson")) |>
+    arrange(match(SampleID, colnames(raw_counts)))
+
+count_matrix <- raw_counts |>
+    select(Geneid, all_of(sm_info$SampleID)) |>
+    tibble::column_to_rownames("Geneid") |>
+    as.matrix()
+
+sm_info <- sm_info |>
+    slice(match(colnames(count_matrix), SampleID)) |>
+    tibble::column_to_rownames("SampleID")
+
+dds_sm <- DESeqDataSetFromMatrix(
+    countData = count_matrix,
+    colData = sm_info,
+    design = ~ County
+)
+dds_sm <- DESeq(dds_sm)
+
+county_levels <- levels(sm_info$County)
+pairs <- combn(county_levels, 2, simplify = FALSE)
+
+for (pair in pairs) {
+    level_A <- pair[2]
+    level_B <- pair[1]
+
+    res <- results(dds_sm, contrast = c("County", level_A, level_B))
+
+    res_df <- as.data.frame(res) |>
+        tibble::rownames_to_column("Geneid") |>
+        arrange(padj)
+
+    label <- paste0(gsub(" ", "", level_A), "_vs_", gsub(" ", "", level_B))
+    out_file <- paste0(output, "_", label, ".csv")
+
+    write.csv(res_df, out_file, row.names = FALSE)
+}
--- 2_DGE_Project/scripts/run_analysis.sh
+++ 2_DGE_Project/scripts/run_analysis.sh
@@ -42,17 +42,44 @@ fi
 
 
 #run a test on the localities, takes 3 arguments: featurecounts output, csv with sample data, and output path + name
-#run marm analysis
+#run marm analysis for localities using LRT
 # Rscript localities_analysis.r "$WORKDIR/analysis/marm_counts.txt" "$WORKDIR/analysis/SampleInfo.csv" "$WORKDIR/analysis/marm_localities.csv"
-Rscript dge_R_volcano_plot.R "$WORKDIR/analysis/marm_localities.csv" "Gene expression differences between localities" "$WORKDIR/analysis/marm_localities_volcano.pdf"
+# Rscript dge_R_volcano_plot.R "$WORKDIR/analysis/marm_localities.csv" "Gene expression differences between localities" "$WORKDIR/analysis/marm_localities_volcano.pdf"
 
+#run marm analysis for localities using a pairwise test
+# Rscript pair_wise_localities.r "$WORKDIR/analysis/marm_counts.txt" "$WORKDIR/analysis/SampleInfo.csv" "$WORKDIR/analysis/marm_localities"
+Rscript dge_R_volcano_plot.R "$WORKDIR/analysis/marm_localities_SantaFe_vs_Brewster.csv" "Gene expression SantaFe vs Brewster" "$WORKDIR/analysis/marm_santafe_vs_brewster_volcano.pdf"
+Rscript dge_R_volcano_plot.R "$WORKDIR/analysis/marm_localities_Brewster_vs_Culberson.csv" "Gene expression Brewster vs Culberson" "$WORKDIR/analysis/marm_brewster_vs_culberson_volcano.pdf"
+Rscript dge_R_volcano_plot.R "$WORKDIR/analysis/marm_localities_SantaFe_vs_Culberson.csv" "Gene expression SantaFe vs Culberson" "$WORKDIR/analysis/marm_santafe_vs_culberson_volcano.pdf"
+
+
+#marm analysis for tissue types using LRT
 # Rscript tissue_type_analysis.r "$WORKDIR/analysis/marm_counts.txt" "$WORKDIR/analysis/SampleInfo.csv" "$WORKDIR/analysis/marm_tissues.csv"
-Rscript dge_R_volcano_plot.R "$WORKDIR/analysis/marm_tissues.csv" "Gene expression differences between tissue types" "$WORKDIR/analysis/marm_tissues_volcano.pdf"
+# Rscript dge_R_volcano_plot.R "$WORKDIR/analysis/marm_tissues.csv" "Gene expression differences between tissue types" "$WORKDIR/analysis/marm_tissues_volcano.pdf"
 
-#run sept analysis
+#run sept analysis for tissue type using pairwise test
+# Rscript pair_wise_tissues.r "$WORKDIR/analysis/marm_counts.txt" "$WORKDIR/analysis/SampleInfo.csv" "$WORKDIR/analysis/marm_tissues"
+Rscript dge_R_volcano_plot.R "$WORKDIR/analysis/marm_tissues_H_vs_SM.csv" "Gene expression Heart vs Skeletal Muscle" "$WORKDIR/analysis/marm_H_vs_SM_volcano.pdf"
+Rscript dge_R_volcano_plot.R "$WORKDIR/analysis/marm_tissues_L_vs_H.csv" "Gene expression Liver vs Heart" "$WORKDIR/analysis/marm_L_vs_H_volcano.pdf"
+Rscript dge_R_volcano_plot.R "$WORKDIR/analysis/marm_tissues_L_vs_SM.csv" "Gene expression Liver vs Skeletal Muscle" "$WORKDIR/analysis/marm_L_vs_SM_volcano.pdf"
+
+
+#run sept analysis for localities using LRT
 # Rscript localities_analysis.r "$WORKDIR/analysis/sept_counts.txt" "$WORKDIR/analysis/SampleInfo.csv" "$WORKDIR/analysis/sept_localities.csv"
-Rscript dge_R_volcano_plot.R "$WORKDIR/analysis/sept_localities.csv" "Gene expression differences between localities" "$WORKDIR/analysis/sept_localities_volcano.pdf"
+# Rscript dge_R_volcano_plot.R "$WORKDIR/analysis/sept_localities.csv" "Gene expression differences between localities" "$WORKDIR/analysis/sept_localities_volcano.pdf"
 
+#run sept analysis for localities using pairwise testing
+# Rscript pair_wise_localities.r "$WORKDIR/analysis/sept_counts.txt" "$WORKDIR/analysis/SampleInfo.csv" "$WORKDIR/analysis/sept_localities"
+Rscript dge_R_volcano_plot.R "$WORKDIR/analysis/sept_localities_SantaFe_vs_Brewster.csv" "Gene expression SantaFe vs Brewster" "$WORKDIR/analysis/sept_santafe_vs_brewster_volcano.pdf"
+Rscript dge_R_volcano_plot.R "$WORKDIR/analysis/sept_localities_Brewster_vs_Culberson.csv" "Gene expression Brewster vs Culberson" "$WORKDIR/analysis/sept_brewster_vs_culberson_volcano.pdf"
+Rscript dge_R_volcano_plot.R "$WORKDIR/analysis/sept_localities_SantaFe_vs_Culberson.csv" "Gene expression SantaFe vs Culberson" "$WORKDIR/analysis/sept_santafe_vs_culberson_volcano.pdf"
+
+#run sept analysis for tissue type using LRT
 # Rscript tissue_type_analysis.r "$WORKDIR/analysis/sept_counts.txt" "$WORKDIR/analysis/SampleInfo.csv" "$WORKDIR/analysis/sept_tissues.csv"
-Rscript dge_R_volcano_plot.R "$WORKDIR/analysis/sept_tissues.csv" "Gene expression differences between tissue types" "$WORKDIR/analysis/sept_tissues_volcano.pdf"
+# Rscript dge_R_volcano_plot.R "$WORKDIR/analysis/sept_tissues.csv" "Gene expression differences between tissue types" "$WORKDIR/analysis/sept_tissues_volcano.pdf"
 
+#run sept analysis for tissue type using pairwise test
+# Rscript pair_wise_tissues.r "$WORKDIR/analysis/sept_counts.txt" "$WORKDIR/analysis/SampleInfo.csv" "$WORKDIR/analysis/sept_tissues"
+Rscript dge_R_volcano_plot.R "$WORKDIR/analysis/sept_tissues_H_vs_SM.csv" "Gene expression Heart vs Skeletal Muscle" "$WORKDIR/analysis/sept_H_vs_SM_volcano.pdf"
+Rscript dge_R_volcano_plot.R "$WORKDIR/analysis/sept_tissues_L_vs_H.csv" "Gene expression Liver vs Heart" "$WORKDIR/analysis/sept_L_vs_H_volcano.pdf"
+Rscript dge_R_volcano_plot.R "$WORKDIR/analysis/sept_tissues_L_vs_SM.csv" "Gene expression Liver vs Skeletal Muscle" "$WORKDIR/analysis/sept_L_vs_SM_volcano.pdf"
